2c087e3f15ba9a945abb9e46aba94f5608c0e1ba
garaba <feld.mariana@gmail.com>
2015-05-24 10:47:05 -0300

    PA2 Garaba

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function  is able to cache potentially time-consuming inversion of a matrix
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x  ## Get matrix
+  setinv <- function(solve) m <<- solve(x)  ## Invert matrix
+  getinv<- function() m  ## Get computed inverted matrix, imatrix
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the matrix returned by the above function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()  ##Gets the imatrix from cache
+    if(!is.null(m)) {   ##Checks imatrix existence in cache
+      message("getting cached data")
+      return(m)   ##Returns the imatrix from cache
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)  ##Stores imatrix in cache
+        m    ## Returns imatrix, the inverse of 'x'
 }
